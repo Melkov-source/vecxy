4767c6b5f23a2deab9a3d8f61f949b38fd6df7d8
Dmitry Melkov <melkov@itibsoft.ru>
2025-03-05 23:13:34 +0300

    - feat: create server and build scripts for logic testing

--- engine/internal/shaders/2d.vert.glsl
+++ engine/internal/shaders/2d.vert.glsl
@@ -14,7 +14,7 @@ void main() {
 
     vec2 zero_to_two = zero_to_one * 2.0;
 
-    vec2 clip_space = zero_to_two - 1.0;
+    vec2 clip_space = (zero_to_two - 1.0) * vec2(1, -1);
 
     gl_Position = vec4(clip_space, 0, 1);
 }
--- game/scripts/build.sh
+++ game/scripts/build.sh
@@ -6,40 +6,24 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ENGINE_DIR="$SCRIPT_DIR/../../engine"
 GAME_DIR="$SCRIPT_DIR/.."
 
-# Function to build the engine
 build_engine() {
     echo "Building Engine..."
     pushd "$ENGINE_DIR" > /dev/null
-    # npm install
     npm run build
     popd > /dev/null
     echo "Engine built successfully."
 }
 
-# Function to build the game
 build_game() {
     echo "Building Game..."
     pushd "$GAME_DIR" > /dev/null
-    # npm install
     npm run build
     popd > /dev/null
     echo "Game built successfully."
 }
 
-run_game() {
-    echo "Running Game..."
-    pushd "$GAME_DIR" > /dev/null
-    npm run start
-    popd > /dev/null
-    echo "Game runned successfully."
-}
-
 # Run build steps
 build_engine
 build_game
-run_game
 
 echo "Build process completed."
-
-# Wait for user input before closing
-read -r -p "Press Enter to exit..."
