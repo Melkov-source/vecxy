d5508e2021db88669f1483638f1a56ce96610d9b
Dmitry Melkov <melkov@itibsoft.ru>
2025-03-08 17:45:30 +0300

    - feat: update color class for 8-bit 255

--- engine/src/graphics/color.ts
+++ engine/src/graphics/color.ts
@@ -1,19 +1,31 @@
 export class Color {
-    public r: number;
-    public g: number;
-    public b: number;
-    public a: number;
+    private _r: number = 0;
+    private _g: number = 0;
+    private _b: number = 0;
+    private _a: number = 0;
 
-    public static white(): Color { return new Color(1, 1, 1); }
+    public static white(): Color { return new Color(255, 255, 255); }
     public static black(): Color { return new Color(0, 0, 0); }
-    public static red(): Color { return new Color(1, 0, 0); }
-    public static green(): Color { return new Color(0, 1, 0); }
-    public static blue(): Color { return new Color(0, 0, 1); }
+    public static red(): Color { return new Color(255, 0, 0); }
+    public static green(): Color { return new Color(0, 255, 0); }
+    public static blue(): Color { return new Color(0, 0, 255); }
 
-    public constructor(r: number, g: number, b: number, a: number = 1) {
-        this.r = r;
-        this.g = g;
-        this.b = b;
-        this.a = a;
+    public constructor(r: number, g: number, b: number, a: number = 255) {
+        this._r = Math.round(r);
+        this._g = Math.round(g);
+        this._b = Math.round(b);
+        this._a = Math.round(a);
     }
+
+    public r0255(): number { return this._r; }
+    public r01(): number { return this._r / 255; }
+
+    public g0255(): number { return this._b; }
+    public g01(): number { return this._b / 255; }
+
+    public b0255(): number { return this._b; }
+    public b01(): number { return this._b / 255; }
+
+    public a0255(): number { return this._a; }
+    public a01(): number { return this._a / 255; }
 }
